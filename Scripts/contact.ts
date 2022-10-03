29bc59a47a8ccda64cf1d074afa7635a28a0bd33
David Janssen <djanssen2112@gmail.com>
2022-10-02 20:47:02 -0400

    added toJSON and from JSON methods to Contact class

--- Scripts/contact.ts
+++ Scripts/contact.ts
@@ -55,12 +55,37 @@ class Contact {
 	 * @return {*}  {string}
 	 * @memberof Contact
 	 */
+
 	public toString(): string {
 		let outputString = "";
 		outputString += `Full Name: ${this.FullName}\n`;
 		outputString += `Contact Number: ${this.ContactNumber}\n`;
 		outputString += `Email Address: ${this.EmailAddress}\n`;
 		return outputString;
+	}
+
+	/**
+	 * This method converts class Data Members to a comma-separated list compatible with JSON
+	 *
+	 * @return {*}  {string}
+	 * @memberof Contact
+	 */
+
+	public toJSON(): string {
+		return `${this.FullName}, ${this.ContactNumber}, ${this.EmailAddress}}`;
+	}
+
+	/**
+	 * This method reads data from a comma-separated list and assigns it to class Data members
+	 *
+	 * @param {string} data
+	 * @memberof Contact
+	 */
+
+	public fromJSON(data: any): void {
+		this.FullName = data.FullName;
+		this.ContactNumber = data.ContactNumber;
+		this.EmailAddress = data.EmailAddress;
 	}
 	//Private methods
 }
